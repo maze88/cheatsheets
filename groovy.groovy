6c9f37fd45a4598a66d588d1ff52323cd16bf77a
Michael Zeevi <michael.52@msn.com>
2021-08-24 01:21:04 +0300

    gpg and order

--- groovy.groovy
+++ groovy.groovy
@@ -1,11 +1,13 @@
 // jenkins script console
 Jenkins.instance.getItemByFullName("JOBNAME").updateNextBuildNumber(666)  // input in 'manage jenkins'->'script console'. changes the next build's number to 666 for job JOBNAME.
 println "Job type: ${Jenkins.getInstance().getItem("MYJOB").getClass()}"  // input in 'manage jenkins'->'script console'. prints the job type of job MYJOB.
+
 // jenkins pipeline (imperative)
 x = sh(returnStdout: true, script: "echo value").trim()  // capture shell output into var x.
 (x, y, z) = v.tokenize(".")  // split string at dots to variables.
+
 // jenkins pipeline (declerative)
+sh("mvn help:effective-pom -Doutput=/tmp/effective-pom.xml")  // save effective-pom.xml to output file.
 stash(includes: "target/", name: "build_output")  // stashes a workspace artifact (such as "target/") between stages/workers in a jenkins pipeline.
 unstash("build_output")  // retrieves above stashed artifact to current workspace.
-sh("mvn help:effective-pom -Doutput=/tmp/effective-pom.xml")  // save effective-pom.xml to output file.
 
--- k8s.sh
+++ k8s.sh
@@ -1,7 +1,7 @@
 # k8s
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.7/components.yaml  # setup monitoring with metrics-server, then can use `kubectl top ...`.
 kubectl get pods -o custom-columns='NAME:metadata.name,IMAGE:spec.containers[*].image'  # list images used in all containers in all pods.
-kubectl get svcs -o custom-columns='NAME:.metadata.name,PORTS:.spec.ports[*].port' svc  # get all external ports of all services.
+kubectl get svcs -o custom-columns='NAME:metadata.name,PORTS:.spec.ports[*].port' svc  # get all external ports of all services.
 kubectl patch configmap www -p "$(cat patch.yaml)"  # applied changes in patch.yaml to configmap 'www'.
 kubectl wait --for=condition=available deployment web  # wait for deployment 'web' to be available.
 kubectl wait --for=condition=ready pod -l app=foo  # wait for pods with label 'app=foo' to be ready.
@@ -9,7 +9,7 @@ kubectl wait --for=condition=complete job/myjob  # wait for job 'myjob' to be co
 kubectl expose deployment web-deployment --name web-svc --port=80 --target-port=5000  # imperativley create service web-svc for deployment web-deployment.
 kubectl port-forward svc/kibana 5601  # expose kibana service over localhost:5601.
 kubectl port-forward PODNAME HOSTPORT:PODPORT  # forward a port from within a pod (its container) to my localhost.
-kubectl exec -it mongodb-7749784fc4-98x2s -- mongodump -u mongo -p password --archive --gzip | aws s3 cp - s3://my-mongo-dumps/$(date +'%Y-%m-%d').tar.gz --storage-class STANDARD_IA --sse  # backup mongo tables and save to s3 bucket.
+kubectl exec -it mongodb-123-xyz -- mongodump -u mongo -p password --archive --gzip | aws s3 cp - s3://my-mongo-dumps/$(date +'%Y-%m-%d').tar.gz --storage-class STANDARD_IA --sse  # backup mongo tables and save to s3 bucket.
 
 # minikube
 eval $(minikube docker-env)  # exports the minikube VM to local env, in which we can (for instance) build a docker image directly into minikube.
