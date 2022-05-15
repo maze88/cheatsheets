e8eac55f1f34bc5b7901df4ab471504f2d654591
Michael Zeevi <michaelzeevi@protonmail.com>
2022-05-15 18:23:03 +0300

    added ip address validation

--- python.py
+++ python.py
@@ -1,4 +1,8 @@
 # logging
 import logging
-logging.basicConfig(filename='demo-app.log', format='%(asctime)s:%(levelname)s:%(funcName)s:%(message)s')
-logging.error('failed to connect')
+logging.basicConfig(filename="app.log", format="%(asctime)s:%(levelname)s:%(funcName)s:%(message)s")
+logging.error("failed to connect")
+
+# ip address validation
+import ipaddress
+ipaddress.ip_address("${ip}")
